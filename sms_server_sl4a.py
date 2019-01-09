dc173c61ad138dcbc062b9a61ab1996792579916
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-01-09 23:37:41 +0330

    Add files via upload

---
+++ sms_server_sl4a.py
@@ -0,0 +1,47 @@
+#!flask/bin/python
+
+'''
+Android SMS server
+SL4A version
+By NimaH79
+NimaH79.ir
+'''
+
+from flask import Flask, jsonify, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+import androidhelper
+
+app = Flask(__name__, static_url_path='')
+api = Api(app)
+droid = androidhelper.Android()
+
+
+class sendSms(Resource):
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'phone',
+            type=str,
+            required=True,
+            help='No phone number provided')
+        self.reqparse.add_argument(
+            'text',
+            type=str,
+            required=True,
+            help='No text provided')
+        super(sendSms, self).__init__()
+
+    def get(self):
+        return {'message': 'you should use POST method'}, 405
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        droid.smsSend(args['phone'], args['text'])
+        return {'message': 'success'}
+
+
+api.add_resource(sendSms, '/sendSMS', endpoint='sendSMS')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
